a2af16b0537e5a15e5140400d2c09316200ea73c
Gisele7 <giselecidral4@gmail.com>
2025-10-05 11:29:48 -0300

    feat(main): add git workflow

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './shared/filters/httpException.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter())
-  await app.listen(Number(process.env.PORT) || 3000);
+  await app.listen(Number(process.env.PORT) || 3010);
 }
 bootstrap();
