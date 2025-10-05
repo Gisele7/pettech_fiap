337cad5a6294be711f942f9bd9e4d93ccac1c91a
Gisele7 <giselecidral4@gmail.com>
2025-10-05 12:00:37 -0300

    feat(main): add hello docker controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHelloDocker(): string {
+    return 'Hello Docker'
   }
 }
